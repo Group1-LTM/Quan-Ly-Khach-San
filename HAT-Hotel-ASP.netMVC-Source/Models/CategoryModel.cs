e98f0a5bf60fffa88d37a86bd3b611831768d3b1
Thanghoanghnue <35320170+Thanghoanghnue@users.noreply.github.com>
2018-03-07 15:02:55 +0700

    Upload Source Code Khách Sạn sử dụng ASP.Net MVC 5

---
+++ HAT-Hotel-ASP.netMVC-Source/Models/CategoryModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Framework;
+using Models;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace Models
+{
+    public class CategoryModel
+    {
+        private PiccatinhDbcontext context = null;
+        public CategoryModel()
+        {
+            context = new PiccatinhDbcontext();
+        }
+        public List<Category> ListAll()
+        {
+            var list = context.Database.SqlQuery<Category>("Sp_Category_ListAll").ToList();
+            return list;
+        }
+        public int Create(int id, string name, string alias, int? parentID, int? order, bool? status)
+        {
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@ID",id),
+                new SqlParameter("@Name",name),
+                new SqlParameter("@Alias",alias),
+                new SqlParameter("@ParentID", parentID),
+                new SqlParameter("@Order", order),
+                new SqlParameter("@Status", status)
+        };
+        int res = context.Database.ExecuteSqlCommand("Category_Insert @ID,@Name, @Alias, @ParentID,@Order,@Status",parameters);
+            return res;
+        }
+}
+}
---
+++ HAT-Hotel-ASP.netMVC-Source/piccatinh.com/Areas/Administrator/Controllers/CategoryController.cs
@@ -0,0 +1,107 @@
+﻿using Models;
+using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace piccatinh.com.Areas.Administrator.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        // GET: Administrator/Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var iplCate = new CategoryModel();
+            var model = iplCate.ListAll();
+            return View(model);
+        }
+
+        // GET: Administrator/Category/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Administrator/Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administrator/Category/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    var model = new CategoryModel();
+                    int res = model.Create(collection.ID,collection.Name, collection.Alias, collection.ParentID, collection.Order, collection.Status);
+                    if (res > 0)
+                        return RedirectToAction("Index");
+                    else
+                    {
+                        ModelState.AddModelError("", "Thao tác đã xảy ra lỗi");
+                    }
+                }
+                return View(collection);
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Administrator/Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Administrator/Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Administrator/Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Administrator/Category/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
