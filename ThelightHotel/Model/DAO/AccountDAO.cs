a2ba2046b629bb2cb32817ac8357a339189db70b
Thanghoanghnue <35320170+Thanghoanghnue@users.noreply.github.com>
2018-03-08 21:59:42 +0700

    Upload Sourcode Khach san theo ASP.Net MVC5

---
+++ ThelightHotel/Model/DAO/AccountDAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+namespace Model.DAO
+{
+    public class AccountDAO
+
+    {
+        ThelightHotelDbContext db = null;
+        public AccountDAO()
+        {
+            db = new ThelightHotelDbContext();
+        }
+        public long Insert(Account entity)
+        {
+            db.Accounts.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+
+
+        }
+        public bool Login(string username, string password)
+        {
+            var result = db.Accounts.Count(x => x.UserName == username && x.Password == password);
+            if (result > 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
